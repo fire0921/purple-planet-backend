8aa068af0b4d20a153b1f0f273c237e5381f88c8
tim_lin <tim.lin@ucfunnel.com>
2019-04-27 19:13:28 +0800

    [test]login api

--- controllers/login.js
+++ controllers/login.js
@@ -33,33 +33,3 @@ exports.login_api_controller = function(req, res){
 		});
 	});
 };
-exports.registered_api_controller = function(req, res){
-	MongoClient.connect(url, {useNewUrlParser: true}, (err, client) => {
-		if(err){
-			console.log(err);
-			return;
-		}
-		return new Promise((resolve, reject) => {
-			const testdb = client.db("test").collection("test");
-			const result = testdb.findOne();
-			if(result){
-				resolve(result);
-			}else{
-				const err = new Error("has some error ocurred!!!");
-				reject(err);
-			}
-		}).then((msg) => {
-			if(msg){
-				console.log(msg);
-				res.json(msg);
-			}else{
-				const err = new Error("No result!!");
-				throw err;
-			}
-		}).catch((err) => {
-			console.log(err.message);
-		}).then(() => {
-			client.close();
-		});
-	});
-};
